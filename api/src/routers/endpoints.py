ebc98e6742cfe2d1294e321189b006347477decd
Samuel Huestis <samhuestis@gmail.com>
2025-04-28 16:02:31 -0600

    feat: endpoint for scenarios by scenario_id (#49)

--- api/src/routers/endpoints.py
+++ api/src/routers/endpoints.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import inspect
+from sqlalchemy.orm import Session
+from models.sql_models import Scenario
 from services.db import get_db, engine
 
 
 table_router = APIRouter()
 
 
+# Router to get all tables in the database
 @table_router.get("/tables")
 def get_tables(db=Depends(get_db)):
     return {"tables": get_tables_from_db()}
@@ -17,3 +20,13 @@ def get_tables_from_db():
     return inspector.get_table_names(
         schema="pbtar"
     )  # Get table names in the 'pbtar' schema
+
+
+# Router to get scenarios by scenario_id
+@table_router.get("/scenarios/{scenario_id}")
+def get_scenario_by_id(scenario_id: int, db: Session = Depends(get_db)):
+    # Query the Scenarios table by primary key (scenario_id)
+    scenario = db.query(Scenario).get(scenario_id)
+    if not scenario:
+        raise HTTPException(status_code=404, detail="Scenario not found")
+    return scenario
