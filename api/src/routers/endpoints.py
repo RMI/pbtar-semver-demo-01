RMI/pbtar-semver-demo-01

commit 4adc3e44a9b70d1ed9d869cdfd537cba8f265e8f

feat: added organization routers (#61)
* added organization routers

* Update api/src/routers/endpoints.py

Co-authored-by: Jackson Hoffart <jackson.hoffart@gmail.com>

* Update api/src/routers/endpoints.py

Co-authored-by: Jackson Hoffart <jackson.hoffart@gmail.com>

* Update api/src/routers/endpoints.py

Co-authored-by: Jackson Hoffart <jackson.hoffart@gmail.com>

---------

Co-authored-by: Jackson Hoffart <jackson.hoffart@gmail.com>

api/src/routers/endpoints.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import inspect
 from sqlalchemy.orm import Session
-from models.sql_models import Scenario
+from models.sql_models import Scenario, Organization
 from services.db import get_db, engine
 
 
-table_router = APIRouter()
+endpoints = APIRouter()
 
 
 # Router to get all tables in the database
-@table_router.get("/tables")
+@endpoints.get("/tables")
 def get_tables(db=Depends(get_db)):
     return {"tables": get_tables_from_db()}
 
@@ -23,7 +23,7 @@ def get_tables_from_db():
 
 
 # Router to get the entire scenarios table
-@table_router.get("/scenarios")
+@endpoints.get("/scenarios")
 def get_scenarios(db: Session = Depends(get_db)):
     # Query the scenarios
     scenarios = db.query(Scenario).all()
@@ -33,10 +33,30 @@ def get_scenarios(db: Session = Depends(get_db)):
 
 
 # Router to get scenarios by scenario_id
-@table_router.get("/scenarios/{scenario_id}")
+@endpoints.get("/scenarios/{scenario_id}")
 def get_scenario_by_id(scenario_id: int, db: Session = Depends(get_db)):
     # Query the Scenarios table by primary key (scenario_id)
     scenario = db.query(Scenario).get(scenario_id)
     if not scenario:
         raise HTTPException(status_code=404, detail="Scenario not found")
     return scenario
+
+
+# Router to get the entire organizations table
+@endpoints.get("/organizations")
+def get_organization(db: Session = Depends(get_db)):
+    # Query the organizations
+    organizations = db.query(Organization).all()
+    if not organizations:
+        raise HTTPException(status_code=404, detail="Organization not found")
+    return organizations
+
+
+# Router to get organization by organization_id
+@endpoints.get("/organizations/{organization_id}")
+def get_scenario_by_id(organization_id: int, db: Session = Depends(get_db)):
+    # Query the Organizations table by primary key (organization_id)
+    organization = db.query(Organization).get(organization_id)
+    if not organization:
+        raise HTTPException(status_code=404, detail="Organization not found")
+    return organization

api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from src.routers.health import health_router
 from src.routers.mtcars import data_output
-from src.routers.endpoints import table_router
+from src.routers.endpoints import endpoints
 from fastapi.middleware.cors import CORSMiddleware
 
 import uvicorn
@@ -56,7 +56,7 @@ async def redirect():
 
 app.include_router(health_router)
 app.include_router(data_output)
-app.include_router(table_router)
+app.include_router(endpoints)
 
 
 if __name__ == "__main__":

commit 476f3e9bbf9ce02cdedcfaec411ee63d51eb63b1

fix: name `get_organization_by_id` function correctly (#64)
The function was erroneously defined as `get_scenario_by_id`, which is a
function name already used for a different endpoint.

The typo was introduced in #61.

api/src/routers/endpoints.py
@@ -54,7 +54,7 @@ def get_organization(db: Session = Depends(get_db)):
 
 # Router to get organization by organization_id
 @endpoints.get("/organizations/{organization_id}")
-def get_scenario_by_id(organization_id: int, db: Session = Depends(get_db)):
+def get_organization_by_id(organization_id: int, db: Session = Depends(get_db)):
     # Query the Organizations table by primary key (organization_id)
     organization = db.query(Organization).get(organization_id)
     if not organization: