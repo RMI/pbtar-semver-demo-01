d64109f3c39b1d713c34a3e19db40f30295536f8
CJ Yetman <cj@cjyetman.com>
2025-06-04 12:20:17 +0200

    split scenario metadata into separate files (#162)
    intended to move towards a goal of having an individual JSON file for
    each scenario, to facilitate more easy management and addition of new
    scenarios

--- src/data/scenariosData.ts
+++ src/data/scenariosData.ts
@@ -1,4 +1,21 @@
 import { Scenario } from "../types";
 
-import scenarios_metadata from "./scenarios_metadata.json" with { type: "json" };
-export const scenariosData: Scenario[] = scenarios_metadata;
+import scenarios_metadata_1 from "./scenarios_metadata_1.json" with { type: "json" };
+import scenarios_metadata_2 from "./scenarios_metadata_2.json" with { type: "json" };
+import scenarios_metadata_3 from "./scenarios_metadata_3.json" with { type: "json" };
+import scenarios_metadata_4 from "./scenarios_metadata_4.json" with { type: "json" };
+import scenarios_metadata_5 from "./scenarios_metadata_5.json" with { type: "json" };
+import scenarios_metadata_6 from "./scenarios_metadata_6.json" with { type: "json" };
+import scenarios_metadata_7 from "./scenarios_metadata_7.json" with { type: "json" };
+import scenarios_metadata_8 from "./scenarios_metadata_8.json" with { type: "json" };
+
+export const scenariosData: Scenario[] = [
+  ...scenarios_metadata_1,
+  ...scenarios_metadata_2,
+  ...scenarios_metadata_3,
+  ...scenarios_metadata_4,
+  ...scenarios_metadata_5,
+  ...scenarios_metadata_6,
+  ...scenarios_metadata_7,
+  ...scenarios_metadata_8,
+];
