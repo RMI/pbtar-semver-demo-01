45edd6d39bf880ed5809aac3a88d59ba02ea5dbb
Alex Axthelm <aaxthelm@RMI.org>
2025-06-26 11:54:46 +0200

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ const Header: React.FC = () => {
           />
           <div>
             <h1 className="text-xl md:text-2xl font-bold tracking-tight">
-              Climate Transition Scenarios Repository
+              Semver Demo!
             </h1>
             <p className="text-xs md:text-sm text-white">by RMI</p>
           </div>
